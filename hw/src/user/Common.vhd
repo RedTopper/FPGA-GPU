e749f1435846d802716caaa7ad90395632897a2a
ajwalter <ajwalter@iastate.edu>
2021-02-03 11:41:15 -0600

    code cleanup and addition fixes

--- hw/src/user/Common.vhd
+++ hw/src/user/Common.vhd
@@ -6,4 +6,13 @@ package Common is
     TYPE uint16_1x4array IS ARRAY(0 TO 3) OF unsigned(15 DOWNTO 0);
     TYPE uint16_4x4array IS ARRAY(0 TO 3) OF uint16_1x4array;
 
+    TYPE uint64_1x4array IS ARRAY(0 TO 3) OF unsigned(63 DOWNTO 0);
+    TYPE uint64_4x4array IS ARRAY(0 TO 3) OF uint64_1x4array;
+
+    TYPE std64_1x4array IS ARRAY(0 TO 3) OF STD_LOGIC_VECTOR(63 DOWNTO 0);
+    TYPE std64_4x4array IS ARRAY(0 TO 3) OF std64_1x4array;
+
+    TYPE addr15_8array IS ARRAY(0 TO 7) OF STD_LOGIC_VECTOR(14 DOWNTO 0);
+    TYPE addr32_8array IS ARRAY(0 TO 7) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
+
 end Common; 
--- hw/src/user/user_logic.vhd
+++ hw/src/user/user_logic.vhd
@@ -58,21 +58,16 @@ ARCHITECTURE mixed OF user_logic IS
 	SIGNAL s_CNT : unsigned(128 DOWNTO 0);
 
 	-- Signals to interface with the dmem component
-	SIGNAL s_ADDRa, s_ADDRb, s_ADDRc, s_ADDRd, s_ADDRe, s_ADDRf, s_ADDRg, s_ADDRh : STD_LOGIC_VECTOR(14 DOWNTO 0);
-	SIGNAL s_RDATAa, s_RDATAb, s_RDATAc, s_RDATAd, s_RDATAe, s_RDATAf, s_RDATAg, s_RDATAh : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_ADDR : addr15_8array;
+	SIGNAL s_RDATA : addr32_8array;
 	SIGNAL s_vectorsRead : unsigned(11 DOWNTO 0);
 
-	SIGNAL s_Y0a, s_Y0b, s_Y0c, s_Y0d : STD_LOGIC_VECTOR(63 DOWNTO 0);
-	SIGNAL s_Y1a, s_Y1b, s_Y1c, s_Y1d : STD_LOGIC_VECTOR(63 DOWNTO 0);
-	SIGNAL s_Y2a, s_Y2b, s_Y2c, s_Y2d : STD_LOGIC_VECTOR(63 DOWNTO 0);
-	SIGNAL s_Y3a, s_Y3b, s_Y3c, s_Y3d : STD_LOGIC_VECTOR(63 DOWNTO 0);
-
-	SIGNAL s_Y0_Summing, s_Y1_Summing, s_Y2_Summing, s_Y3_Summing : STD_LOGIC_VECTOR(63 DOWNTO 0);
-	SIGNAL s_Y0_SUM_TOTAL, s_Y1_SUM_TOTAL, s_Y2_SUM_TOTAL, s_Y3_SUM_TOTAL : STD_LOGIC_VECTOR(63 DOWNTO 0);
+	SIGNAL s_Y : std64_4x4array;
+	SIGNAL s_Y_TOTAL : uint64_4x4array;
 
 	-- Signals to hold the array values
 	SIGNAL s_Amatrix : uint16_4x4array;
-	SIGNAL s_XVECTa, s_XVECTb, s_XVECTc, s_XVECTd : STD_LOGIC_VECTOR(63 DOWNTO 0);
+	SIGNAL s_XVECT : std64_1x4array;
 
 	-- Finite State Machine signals
 	TYPE state_type IS (S0, S1, S2, S3, S4);
@@ -100,22 +95,22 @@ BEGIN
 	PORT MAP(
 		i_CLKa => i_CLK,
 		i_CLKb => i_CLK,
-		i_ADDRa => s_ADDRa,
-		i_ADDRb => s_ADDRb,
-		i_ADDRc => s_ADDRc,
-		i_ADDRd => s_ADDRd,
-		i_ADDRe => s_ADDRe,
-		i_ADDRf => s_ADDRf,
-		i_ADDRg => s_ADDRg,
-		i_ADDRh => s_ADDRh,
-		o_RDATAa => s_RDATAa,
-		o_RDATAb => s_RDATAb,
-		o_RDATAc => s_RDATAc,
-		o_RDATAd => s_RDATAd,
-		o_RDATAe => s_RDATAe,
-		o_RDATAf => s_RDATAf,
-		o_RDATAg => s_RDATAg,
-		o_RDATAh => s_RDATAh);
+		i_ADDRa => s_ADDR(0),
+		i_ADDRb => s_ADDR(1),
+		i_ADDRc => s_ADDR(2),
+		i_ADDRd => s_ADDR(3),
+		i_ADDRe => s_ADDR(4),
+		i_ADDRf => s_ADDR(5),
+		i_ADDRg => s_ADDR(6),
+		i_ADDRh => s_ADDR(7),
+		o_RDATAa => s_RDATA(0),
+		o_RDATAb => s_RDATA(1),
+		o_RDATAc => s_RDATA(2),
+		o_RDATAd => s_RDATA(3),
+		o_RDATAe => s_RDATA(4),
+		o_RDATAf => s_RDATA(5),
+		o_RDATAg => s_RDATA(6),
+		o_RDATAh => s_RDATA(7));
 
 	-- Temporary logic - set the result vector to arbitrary values. 
 	-- o_Y0 <= x"1a1a1a1a2b2b2b2b";
@@ -141,15 +136,14 @@ BEGIN
 			CASE cur_state IS
 					-- When we've reset, we can initialize the s_ADDRa signal
 				WHEN S0 =>
-					s_ADDRa <= (OTHERS => '0');
-					s_ADDRa <= "000000000000000";
-					s_ADDRb <= "000000000000001";
-					s_ADDRc <= "000000000000010";
-					s_ADDRd <= "000000000000011";
-					s_ADDRe <= "000000000000100";
-					s_ADDRf <= "000000000000101";
-					s_ADDRg <= "000000000000110";
-					s_ADDRh <= "000000000000111";
+					s_ADDR(0) <= "000000000000000";
+					s_ADDR(1) <= "000000000000001";
+					s_ADDR(2) <= "000000000000010";
+					s_ADDR(3) <= "000000000000011";
+					s_ADDR(4) <= "000000000000100";
+					s_ADDR(5) <= "000000000000101";
+					s_ADDR(6) <= "000000000000110";
+					s_ADDR(7) <= "000000000000111";
 					cur_state <= S1;
 					s_vectorsRead <= x"000";
 
@@ -158,81 +152,93 @@ BEGIN
 				WHEN S1 =>
 					-- This is the recommended mechanism for doing math operations on 
 					-- std_logic_vectors. 		  
-					s_ADDRa <= STD_LOGIC_VECTOR(unsigned(s_ADDRa) + 8);
-					s_ADDRb <= STD_LOGIC_VECTOR(unsigned(s_ADDRb) + 8);
-					s_ADDRc <= STD_LOGIC_VECTOR(unsigned(s_ADDRc) + 8);
-					s_ADDRd <= STD_LOGIC_VECTOR(unsigned(s_ADDRd) + 8);
-					s_ADDRe <= STD_LOGIC_VECTOR(unsigned(s_ADDRe) + 8);
-					s_ADDRf <= STD_LOGIC_VECTOR(unsigned(s_ADDRf) + 8);
-					s_ADDRg <= STD_LOGIC_VECTOR(unsigned(s_ADDRg) + 8);
-					s_ADDRh <= STD_LOGIC_VECTOR(unsigned(s_ADDRh) + 8);
+					s_ADDR(0) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(0)) + 8);
+					s_ADDR(1) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(1)) + 8);
+					s_ADDR(2) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(2)) + 8);
+					s_ADDR(3) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(3)) + 8);
+					s_ADDR(4) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(4)) + 8);
+					s_ADDR(5) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(5)) + 8);
+					s_ADDR(6) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(6)) + 8);
+					s_ADDR(7) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(7)) + 8);
 					cur_state <= S2;
 
 					-- We are grabbing two unit16's per 32-bit BRAM read
 				WHEN S2 =>
-					s_Amatrix(0)(0) <= unsigned(s_RDATAa(31 DOWNTO 16));
-					s_Amatrix(0)(1) <= unsigned(s_RDATAa(15 DOWNTO 0));
-					s_Amatrix(0)(2) <= unsigned(s_RDATAb(31 DOWNTO 16));
-					s_Amatrix(0)(3) <= unsigned(s_RDATAb(15 DOWNTO 0));
-					s_Amatrix(1)(0) <= unsigned(s_RDATAc(31 DOWNTO 16));
-					s_Amatrix(1)(1) <= unsigned(s_RDATAc(15 DOWNTO 0));
-					s_Amatrix(1)(2) <= unsigned(s_RDATAd(31 DOWNTO 16));
-					s_Amatrix(1)(3) <= unsigned(s_RDATAd(15 DOWNTO 0));
-					s_Amatrix(2)(0) <= unsigned(s_RDATAe(31 DOWNTO 16));
-					s_Amatrix(2)(1) <= unsigned(s_RDATAe(15 DOWNTO 0));
-					s_Amatrix(2)(2) <= unsigned(s_RDATAf(31 DOWNTO 16));
-					s_Amatrix(2)(3) <= unsigned(s_RDATAf(15 DOWNTO 0));
-					s_Amatrix(3)(0) <= unsigned(s_RDATAg(31 DOWNTO 16));
-					s_Amatrix(3)(1) <= unsigned(s_RDATAg(15 DOWNTO 0));
-					s_Amatrix(3)(2) <= unsigned(s_RDATAh(31 DOWNTO 16));
-					s_Amatrix(3)(3) <= unsigned(s_RDATAh(15 DOWNTO 0));
+					s_Amatrix(0)(0) <= unsigned(s_RDATA(0)(31 DOWNTO 16));
+					s_Amatrix(0)(1) <= unsigned(s_RDATA(0)(15 DOWNTO 0));
+					s_Amatrix(0)(2) <= unsigned(s_RDATA(1)(31 DOWNTO 16));
+					s_Amatrix(0)(3) <= unsigned(s_RDATA(1)(15 DOWNTO 0));
+					s_Amatrix(1)(0) <= unsigned(s_RDATA(2)(31 DOWNTO 16));
+					s_Amatrix(1)(1) <= unsigned(s_RDATA(2)(15 DOWNTO 0));
+					s_Amatrix(1)(2) <= unsigned(s_RDATA(3)(31 DOWNTO 16));
+					s_Amatrix(1)(3) <= unsigned(s_RDATA(3)(15 DOWNTO 0));
+					s_Amatrix(2)(0) <= unsigned(s_RDATA(4)(31 DOWNTO 16));
+					s_Amatrix(2)(1) <= unsigned(s_RDATA(4)(15 DOWNTO 0));
+					s_Amatrix(2)(2) <= unsigned(s_RDATA(5)(31 DOWNTO 16));
+					s_Amatrix(2)(3) <= unsigned(s_RDATA(5)(15 DOWNTO 0));
+					s_Amatrix(3)(0) <= unsigned(s_RDATA(6)(31 DOWNTO 16));
+					s_Amatrix(3)(1) <= unsigned(s_RDATA(6)(15 DOWNTO 0));
+					s_Amatrix(3)(2) <= unsigned(s_RDATA(7)(31 DOWNTO 16));
+					s_Amatrix(3)(3) <= unsigned(s_RDATA(7)(15 DOWNTO 0));
 
-					s_ADDRa <= STD_LOGIC_VECTOR(unsigned(s_ADDRa) + 8);
-					s_ADDRb <= STD_LOGIC_VECTOR(unsigned(s_ADDRb) + 8);
-					s_ADDRc <= STD_LOGIC_VECTOR(unsigned(s_ADDRc) + 8);
-					s_ADDRd <= STD_LOGIC_VECTOR(unsigned(s_ADDRd) + 8);
-					s_ADDRe <= STD_LOGIC_VECTOR(unsigned(s_ADDRe) + 8);
-					s_ADDRf <= STD_LOGIC_VECTOR(unsigned(s_ADDRf) + 8);
-					s_ADDRg <= STD_LOGIC_VECTOR(unsigned(s_ADDRg) + 8);
-					s_ADDRh <= STD_LOGIC_VECTOR(unsigned(s_ADDRh) + 8);
+					s_ADDR(0) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(0)) + 8);
+					s_ADDR(1) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(1)) + 8);
+					s_ADDR(2) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(2)) + 8);
+					s_ADDR(3) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(3)) + 8);
+					s_ADDR(4) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(4)) + 8);
+					s_ADDR(5) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(5)) + 8);
+					s_ADDR(6) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(6)) + 8);
+					s_ADDR(7) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(7)) + 8);
 
 					cur_state <= S3;
 
 				WHEN S3 =>
-					s_XVECTa(63 DOWNTO 48) <= s_RDATAa(31 DOWNTO 16);
-					s_XVECTa(47 DOWNTO 32) <= s_RDATAa(15 DOWNTO 0);
-					s_XVECTa(31 DOWNTO 16) <= s_RDATAb(31 DOWNTO 16);
-					s_XVECTa(15 DOWNTO 0) <= s_RDATAb(15 DOWNTO 0);
+					s_XVECT(0)(63 DOWNTO 48) <= s_RDATA(0)(31 DOWNTO 16);
+					s_XVECT(0)(47 DOWNTO 32) <= s_RDATA(0)(15 DOWNTO 0);
+					s_XVECT(0)(31 DOWNTO 16) <= s_RDATA(1)(31 DOWNTO 16);
+					s_XVECT(0)(15 DOWNTO 0)  <= s_RDATA(1)(15 DOWNTO 0);
 
-					s_XVECTb(63 DOWNTO 48) <= s_RDATAc(31 DOWNTO 16);
-					s_XVECTb(47 DOWNTO 32) <= s_RDATAc(15 DOWNTO 0);
-					s_XVECTb(31 DOWNTO 16) <= s_RDATAd(31 DOWNTO 16);
-					s_XVECTb(15 DOWNTO 0) <= s_RDATAd(15 DOWNTO 0);
+					s_XVECT(1)(63 DOWNTO 48) <= s_RDATA(2)(31 DOWNTO 16);
+					s_XVECT(1)(47 DOWNTO 32) <= s_RDATA(2)(15 DOWNTO 0);
+					s_XVECT(1)(31 DOWNTO 16) <= s_RDATA(3)(31 DOWNTO 16);
+					s_XVECT(1)(15 DOWNTO 0)  <= s_RDATA(3)(15 DOWNTO 0);
 
-					s_XVECTc(63 DOWNTO 48) <= s_RDATAe(31 DOWNTO 16);
-					s_XVECTc(47 DOWNTO 32) <= s_RDATAe(15 DOWNTO 0);
-					s_XVECTc(31 DOWNTO 16) <= s_RDATAf(31 DOWNTO 16);
-					s_XVECTc(15 DOWNTO 0) <= s_RDATAf(15 DOWNTO 0);
+					s_XVECT(2)(63 DOWNTO 48) <= s_RDATA(4)(31 DOWNTO 16);
+					s_XVECT(2)(47 DOWNTO 32) <= s_RDATA(4)(15 DOWNTO 0);
+					s_XVECT(2)(31 DOWNTO 16) <= s_RDATA(5)(31 DOWNTO 16);
+					s_XVECT(2)(15 DOWNTO 0)  <= s_RDATA(5)(15 DOWNTO 0);
 
-					s_XVECTd(63 DOWNTO 48) <= s_RDATAg(31 DOWNTO 16);
-					s_XVECTd(47 DOWNTO 32) <= s_RDATAg(15 DOWNTO 0);
-					s_XVECTd(31 DOWNTO 16) <= s_RDATAh(31 DOWNTO 16);
-					s_XVECTd(15 DOWNTO 0) <= s_RDATAh(15 DOWNTO 0);
+					s_XVECT(3)(63 DOWNTO 48) <= s_RDATA(6)(31 DOWNTO 16);
+					s_XVECT(3)(47 DOWNTO 32) <= s_RDATA(6)(15 DOWNTO 0);
+					s_XVECT(3)(31 DOWNTO 16) <= s_RDATA(7)(31 DOWNTO 16);
+					s_XVECT(3)(15 DOWNTO 0)  <= s_RDATA(7)(15 DOWNTO 0);
 
-					s_ADDRa <= STD_LOGIC_VECTOR(unsigned(s_ADDRa) + 8);
-					s_ADDRb <= STD_LOGIC_VECTOR(unsigned(s_ADDRb) + 8);
-					s_ADDRc <= STD_LOGIC_VECTOR(unsigned(s_ADDRc) + 8);
-					s_ADDRd <= STD_LOGIC_VECTOR(unsigned(s_ADDRd) + 8);
-					s_ADDRe <= STD_LOGIC_VECTOR(unsigned(s_ADDRe) + 8);
-					s_ADDRf <= STD_LOGIC_VECTOR(unsigned(s_ADDRf) + 8);
-					s_ADDRg <= STD_LOGIC_VECTOR(unsigned(s_ADDRg) + 8);
-					s_ADDRh <= STD_LOGIC_VECTOR(unsigned(s_ADDRh) + 8);
+					s_ADDR(0) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(0)) + 8);
+					s_ADDR(1) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(1)) + 8);
+					s_ADDR(2) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(2)) + 8);
+					s_ADDR(3) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(3)) + 8);
+					s_ADDR(4) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(4)) + 8);
+					s_ADDR(5) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(5)) + 8);
+					s_ADDR(6) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(6)) + 8);
+					s_ADDR(7) <= STD_LOGIC_VECTOR(unsigned(s_ADDR(7)) + 8);
 
-					s_Y0_SUM_TOTAL <= STD_LOGIC_VECTOR(unsigned(s_Y0_SUM_TOTAL) + unsigned(s_Y0_Summing));
-					s_Y1_SUM_TOTAL <= STD_LOGIC_VECTOR(unsigned(s_Y1_SUM_TOTAL) + unsigned(s_Y1_Summing));
-					s_Y2_SUM_TOTAL <= STD_LOGIC_VECTOR(unsigned(s_Y2_SUM_TOTAL) + unsigned(s_Y2_Summing));
-					s_Y3_SUM_TOTAL <= STD_LOGIC_VECTOR(unsigned(s_Y3_SUM_TOTAL) + unsigned(s_Y3_Summing));
-
+					s_Y_TOTAL(0)(0) <= s_Y_TOTAL(0)(0) + unsigned(s_Y(0)(0));
+					s_Y_TOTAL(0)(1) <= s_Y_TOTAL(0)(1) + unsigned(s_Y(0)(1));
+					s_Y_TOTAL(0)(2) <= s_Y_TOTAL(0)(2) + unsigned(s_Y(0)(2));
+					s_Y_TOTAL(0)(3) <= s_Y_TOTAL(0)(3) + unsigned(s_Y(0)(3));
+					s_Y_TOTAL(1)(0) <= s_Y_TOTAL(1)(0) + unsigned(s_Y(1)(0));
+					s_Y_TOTAL(1)(1) <= s_Y_TOTAL(1)(1) + unsigned(s_Y(1)(1));
+					s_Y_TOTAL(1)(2) <= s_Y_TOTAL(1)(2) + unsigned(s_Y(1)(2));
+					s_Y_TOTAL(1)(3) <= s_Y_TOTAL(1)(3) + unsigned(s_Y(1)(3));
+					s_Y_TOTAL(2)(0) <= s_Y_TOTAL(2)(0) + unsigned(s_Y(2)(0));
+					s_Y_TOTAL(2)(1) <= s_Y_TOTAL(2)(1) + unsigned(s_Y(2)(1));
+					s_Y_TOTAL(2)(2) <= s_Y_TOTAL(2)(2) + unsigned(s_Y(2)(2));
+					s_Y_TOTAL(2)(3) <= s_Y_TOTAL(2)(3) + unsigned(s_Y(2)(3));
+					s_Y_TOTAL(3)(0) <= s_Y_TOTAL(3)(0) + unsigned(s_Y(3)(0));
+					s_Y_TOTAL(3)(1) <= s_Y_TOTAL(3)(1) + unsigned(s_Y(3)(1));
+					s_Y_TOTAL(3)(2) <= s_Y_TOTAL(3)(2) + unsigned(s_Y(3)(2));
+					s_Y_TOTAL(3)(3) <= s_Y_TOTAL(3)(3) + unsigned(s_Y(3)(3));
+					
 					IF (s_vectorsRead = x"3E8") THEN
 						cur_state <= S4;
 					ELSE
@@ -240,15 +246,22 @@ BEGIN
 					END IF;
 
 				WHEN S4 =>
-					o_Y0 <= s_Y0_SUM_TOTAL;
-					o_Y1 <= s_Y1_SUM_TOTAL;
-					o_Y2 <= s_Y2_SUM_TOTAL;
-					o_Y3 <= s_Y3_SUM_TOTAL;
+					o_Y0 <= STD_LOGIC_VECTOR(s_Y_TOTAL(0)(0) + s_Y_TOTAL(0)(1) + s_Y_TOTAL(0)(2) + s_Y_TOTAL(0)(3));
+					o_Y1 <= STD_LOGIC_VECTOR(s_Y_TOTAL(1)(0) + s_Y_TOTAL(1)(1) + s_Y_TOTAL(1)(2) + s_Y_TOTAL(1)(3));
+					o_Y2 <= STD_LOGIC_VECTOR(s_Y_TOTAL(2)(0) + s_Y_TOTAL(2)(1) + s_Y_TOTAL(2)(2) + s_Y_TOTAL(2)(3));
+					o_Y3 <= STD_LOGIC_VECTOR(s_Y_TOTAL(3)(0) + s_Y_TOTAL(3)(1) + s_Y_TOTAL(3)(2) + s_Y_TOTAL(3)(3));
 					s_DONE <= '1';
 
 				WHEN OTHERS =>
 					cur_state <= S0;
-					s_ADDRa <= (OTHERS => '0');
+					s_ADDR(0) <= (OTHERS => '0');
+					s_ADDR(1) <= (OTHERS => '0');
+					s_ADDR(2) <= (OTHERS => '0');
+					s_ADDR(3) <= (OTHERS => '0');
+					s_ADDR(4) <= (OTHERS => '0');
+					s_ADDR(5) <= (OTHERS => '0');
+					s_ADDR(6) <= (OTHERS => '0');
+					s_ADDR(7) <= (OTHERS => '0');
 
 			END CASE;
 		END IF;
@@ -258,49 +271,44 @@ BEGIN
 	PORT MAP(
 		i_CLK => i_CLK,
 		i_A => s_Amatrix,
-		i_X => s_XVECTa,
+		i_X => s_XVECT(0),
 
-		o_MY0 => s_Y0a,
-		o_MY1 => s_Y1a,
-		o_MY2 => s_Y2a,
-		o_MY3 => s_Y3a);
+		o_MY0 => s_Y(0)(0),
+		o_MY1 => s_Y(0)(1),
+		o_MY2 => s_Y(0)(2),
+		o_MY3 => s_Y(0)(3));
 
 	Math_4CHb : Math_4CH
 	PORT MAP(
 		i_CLK => i_CLK,
 		i_A => s_Amatrix,
-		i_X => s_XVECTb,
+		i_X => s_XVECT(1),
 
-		o_MY0 => s_Y0b,
-		o_MY1 => s_Y1b,
-		o_MY2 => s_Y2b,
-		o_MY3 => s_Y3b);
+		o_MY0 => s_Y(1)(0),
+		o_MY1 => s_Y(1)(1),
+		o_MY2 => s_Y(1)(2),
+		o_MY3 => s_Y(1)(3));
 
 	Math_4CHc : Math_4CH
 	PORT MAP(
 		i_CLK => i_CLK,
 		i_A => s_Amatrix,
-		i_X => s_XVECTc,
+		i_X => s_XVECT(2),
 
-		o_MY0 => s_Y0c,
-		o_MY1 => s_Y1c,
-		o_MY2 => s_Y2c,
-		o_MY3 => s_Y3c);
+		o_MY0 => s_Y(2)(0),
+		o_MY1 => s_Y(2)(1),
+		o_MY2 => s_Y(2)(2),
+		o_MY3 => s_Y(2)(3));
 
 	Math_4CHd : Math_4CH
 	PORT MAP(
 		i_CLK => i_CLK,
 		i_A => s_Amatrix,
-		i_X => s_XVECTd,
+		i_X => s_XVECT(3),
 
-		o_MY0 => s_Y0d,
-		o_MY1 => s_Y1d,
-		o_MY2 => s_Y2d,
-		o_MY3 => s_Y3d);
-
-	s_Y0_Summing <= STD_LOGIC_VECTOR(unsigned(s_Y0a) + unsigned(s_Y0b) + unsigned(s_Y0c) + unsigned(s_Y0d));
-	s_Y1_Summing <= STD_LOGIC_VECTOR(unsigned(s_Y1a) + unsigned(s_Y1b) + unsigned(s_Y1c) + unsigned(s_Y1d));
-	s_Y2_Summing <= STD_LOGIC_VECTOR(unsigned(s_Y2a) + unsigned(s_Y2b) + unsigned(s_Y2c) + unsigned(s_Y2d));
-	s_Y3_Summing <= STD_LOGIC_VECTOR(unsigned(s_Y3a) + unsigned(s_Y3b) + unsigned(s_Y3c) + unsigned(s_Y3d));
+		o_MY0 => s_Y(3)(0),
+		o_MY1 => s_Y(3)(1),
+		o_MY2 => s_Y(3)(2),
+		o_MY3 => s_Y(3)(3));
 
 END mixed;
