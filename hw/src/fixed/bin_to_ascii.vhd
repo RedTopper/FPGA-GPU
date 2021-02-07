a05b757ed4043c5edb9f72398d8dd61940a66237
bjpierre <bjpierre@iastate.edu>
2021-02-06 21:07:44 -0600

    Reverted Fixed Logic to default

--- hw/src/fixed/bin_to_ascii.vhd
+++ hw/src/fixed/bin_to_ascii.vhd
@@ -3,6 +3,8 @@
 -- Department of Electrical and Computer Engineering
 -- Iowa State University
 -------------------------------------------------------------------------
+
+
 -- bin_to_ascii.vhd
 -------------------------------------------------------------------------
 -- DESCRIPTION: This file the a simple binary to ASCII translator, 
@@ -11,43 +13,50 @@
 -- NOTES:
 -- 12/16/20 by JAZ::Design created.
 ------------------------------------------------------------------------
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.ALL;
-ENTITY bin_to_ascii IS
-	GENERIC (N : INTEGER := 32);
-	PORT (
-		i_A : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-		o_F : OUT STD_LOGIC_VECTOR(2 * N - 1 DOWNTO 0));
-END bin_to_ascii;
 
-ARCHITECTURE dataflow OF bin_to_ascii IS
 
-	-- Needed for VHDL wierdness
-	TYPE nibble_array IS ARRAY(0 TO N/4 - 1) OF STD_LOGIC_VECTOR(3 DOWNTO 0);
-	SIGNAL s_A : nibble_array;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-BEGIN
 
-	G1 : FOR i IN 0 TO N/4 - 1 GENERATE
+entity bin_to_ascii is
+  generic(N     : integer := 32);
+  port(i_A      : in std_logic_vector(N-1 downto 0);
+       o_F      : out std_logic_vector(2*N-1 downto 0));
+end bin_to_ascii;
 
-		s_A(i) <= i_A(4 * i + 3 DOWNTO 4 * i);
-		WITH s_A(i) SELECT
-		o_F(8 * i + 7 DOWNTO 8 * i) <= x"30" WHEN "0000",
-		x"31" WHEN "0001",
-		x"32" WHEN "0010",
-		x"33" WHEN "0011",
-		x"34" WHEN "0100",
-		x"35" WHEN "0101",
-		x"36" WHEN "0110",
-		x"37" WHEN "0111",
-		x"38" WHEN "1000",
-		x"39" WHEN "1001",
-		x"61" WHEN "1010",
-		x"62" WHEN "1011",
-		x"63" WHEN "1100",
-		x"64" WHEN "1101",
-		x"65" WHEN "1110",
-		x"66" WHEN "1111",
-		x"7A" WHEN OTHERS;
-	END GENERATE;
-END dataflow;
+architecture dataflow of bin_to_ascii is
+
+  -- Needed for VHDL wierdness
+  type nibble_array is array(0 to N/4-1) of std_logic_vector(3 downto 0);
+  signal s_A : nibble_array;
+
+begin
+
+  G1: for i in 0 to N/4-1 generate
+
+    s_A(i) <= i_A(4*i+3 downto 4*i);
+    with s_A(i) select
+	   o_F(8*i+7 downto 8*i) <= x"30" when "0000",
+		                  x"31" when "0001",
+		                  x"32" when "0010",								
+		                  x"33" when "0011",
+		                  x"34" when "0100",								
+		                  x"35" when "0101",
+		                  x"36" when "0110",								
+		                  x"37" when "0111",
+		                  x"38" when "1000",								
+		                  x"39" when "1001",
+		                  x"61" when "1010",								
+		                  x"62" when "1011",
+		                  x"63" when "1100",								
+		                  x"64" when "1101",								
+		                  x"65" when "1110",								
+		                  x"66" when "1111",
+								x"7A" when others;
+
+
+  end generate;
+
+
+end dataflow;
--- hw/src/fixed/uart_Controller.v
+++ hw/src/fixed/uart_Controller.v
@@ -15,60 +15,58 @@
 //-----------------------------------------------------------------------
 
 
-module uart_Controller(clk100,
-                       rst,
-                       RxD,
-                       TxD,
-                       msg_din,
-                       msg_wren,
-                       msg_afull);
-	
+module uart_Controller(clk100, rst, RxD, TxD, msg_din, msg_wren, msg_afull);
+
 	input 		  clk100;
 	input 		  rst;
-	input         RxD;
-	output        TxD;
+   input         RxD;
+   output        TxD;
 	input [63:0]  msg_din;
 	input         msg_wren;
-	output        msg_afull;
+   output        msg_afull;
 	
 	wire 			  msg_rden, msg_rden_reg, wr_n;
-	wire [7:0]    msg_dout;
+   wire [7:0]    msg_dout;
 	
 	reg           msg_rden_reg_d1;
 	
 	mmu_uart_top UART (.Clk(clk100),
-	.Reset_n(!rst),
-	.TXD(TxD),
-	.RXD(RxD),
-	.ck_div(16'd3472),//16'd289 for 115200
-	.CE_N(1'b0),
-	.WR_N(wr_n),
-	.RD_N(1'b1),
-	.A0(1'b0),
-	.D_IN(msg_dout),
-	.D_OUT(),
-	.RX_full(),
-	.TX_busy_n(tx_busy_n));
-	
+			 .Reset_n(!rst),
+ 			 .TXD(TxD),
+       	 .RXD(RxD),
+			 .ck_div(16'd3472),//16'd289 for 115200
+			 .CE_N(1'b0),
+			 .WR_N(wr_n),
+			 .RD_N(1'b1),
+          .A0(1'b0),
+			 .D_IN(msg_dout),
+          .D_OUT(),
+          .RX_full(),
+          .TX_busy_n(tx_busy_n));	
+
 	fifo MSG_BUFFER (
-	.wr_clk(clk100),
-	.wr_rst(rst),
-	.rd_clk(clk100),
-	.rd_rst(rst),
-	.din(msg_din),
-	.wr_en(msg_wren),
-	.rd_en(msg_rden),
-	.dout(msg_dout),
-	.full(msg_full),
-	.almost_full(msg_afull),
-	.empty(msg_empty),
-	.valid(msg_valid));
-	
-	assign wr_n         = ~(tx_busy_n & msg_valid);
-	assign msg_rden_reg = tx_busy_n & (~msg_empty);
-	assign msg_rden     = msg_rden_reg & ~msg_rden_reg_d1;
-	
-	always @ (posedge clk100) begin
-		msg_rden_reg_d1 <= msg_rden_reg;
+  		.wr_clk(clk100),        
+  		.wr_rst(rst),           
+  		.rd_clk(clk100),        
+  		.rd_rst(rst),           
+  		.din(msg_din),     
+  		.wr_en(msg_wren),    
+  		.rd_en(msg_rden), 
+  		.dout(msg_dout), 
+  		.full(msg_full),
+  		.almost_full(msg_afull), 
+  		.empty(msg_empty), 
+  		.valid(msg_valid));
+
+	assign wr_n = ~(tx_busy_n & msg_valid);
+   assign msg_rden_reg = tx_busy_n & (~msg_empty);	
+	assign msg_rden = msg_rden_reg & ~msg_rden_reg_d1;
+
+   always @ (posedge clk100) begin
+	  	msg_rden_reg_d1 <= msg_rden_reg;
 	end
+
+
+
+
 endmodule
