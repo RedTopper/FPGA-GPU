8e643d0a969f57e4cb956ed13cbcfd294c50f5e9
jhermon <jhermon@iastate.edu>
2021-05-05 13:44:00 -0500

    Updated
    Co-authored-by: DMMunday1217 <DMMunday1217@users.noreply.github.com>

--- SGP/hw/ip/sgp_renderOutput/src/sgp_renderOutput.vhd
+++ SGP/hw/ip/sgp_renderOutput/src/sgp_renderOutput.vhd
@@ -435,7 +435,7 @@ BEGIN
   -- Our framebuffer is currently ARBG, so we have to re-assemble a bit. We only need the integer values now
   -- At least set a unique ID for each synthesis run in the debug register, so we know that we're looking at the most recent IP core
   -- It would also be useful to connect internal signals to this register for software debug purposes
-  renderoutput_debug <= x"00000074";
+  renderoutput_debug <= x"00000075";
   -- A 4-state FSM, where we copy fragments, determine the address and color from the input attributes, 
   -- and generate an AXI Write request based on that data.
   --! fsm_extract
@@ -768,7 +768,7 @@ BEGIN
           mem_rd <= '0';
           IF (mem_ack = '1') THEN
             mem_rd_data_stored <= mem_data_rd;
-            state <= Texture;
+            state <= TEXTURE;
           END IF;
 
           WHEN TEXTURE =>
